eccf09889f8dbe1850f400e43831c700eb11a943
leonard1on <leonardborj@gmail.com>
2017-09-08 15:22:58 -0600

    Comenzar main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Escuadron.h"
 #include "Soldado.h"
 #include "Arquero.h"
-#include "Coraza.h"
+#include "Coraza.h"*/:
 #include "Asesino.h"
 
 using namespace std;
+int menu();
+int main() {
+  vector<Escuadron> escuadrones;
+  int opcion=0;
+  do {
+    opcion=menu();
+    switch (opcion) {
+      case 1:{
 
-int main(int argc, char const *argv[]) {
+      }break;
+
+      case 2:{
+
+      }break;
+
+      default:
+        break;
+    }
+  } while(opcion!=3);
+
 
   return 0;
+}
+
+int menu(){
+  int opcion;
+  cout<<" - Menu - " <<endl
+    <<"1. Crear Escuadron" <<endl
+    <<"2. Simulacion" <<endl
+    <<"3. Salir" <<endl
+    <<"Seleccion una opcion: ";
+  cin>>opcion;
+  cout<<endl;
+
+  return opcion;
 }
